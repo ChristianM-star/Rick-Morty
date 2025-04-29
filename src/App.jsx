85ca13cfc00bdd6f1b760efbbe5d2a56510d0575
Your Name <christianmaffs@gmail.com>
2025-04-30 01:39:32 +0200

    fetching data react : 1st commit

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react'
+
+import Cards from "./components/Cards/Cards"
+import Filters from "./components/Filters/Filters"
+
+function App() {
+
+  let [pageNumber, setPageNumber] = useState(1);
+  let [fetchedData, updateFetctedData] = useState([]);
+  let {info, results} = fetchedData;
+
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+
+
+  useEffect(()=> {
+
+    (async function(){
+
+      let data = await fetch(api).then((res)=> res.json());
+      updateFetctedData(data)
+    })()
+  },[api])
+
+  return (
+    <>
+    <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span> </h1>
+
+    <div className="container">
+      <div className="row">
+        <div className="col-3">
+          <Filters/>
+        </div>
+          <div className="col-8">
+            <div className="row">
+           <Cards results={results}/>
+            </div>
+          </div>
+      </div>
+    </div>
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Cards = ({results}) => {
+    let display;
+
+    if(results){
+        display = results.map((x)=> {
+            let {id, name} = x;
+            return <div key={id} className="col-4">{name}</div>
+        })
+    }else{
+        display = 'No Characters Found :/'
+    }
+
+  return (
+    <>{display}</>
+  )
+}
+
+export default Cards
