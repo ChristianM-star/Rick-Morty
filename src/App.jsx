0fab88224d07fadac602696b0af947b24ad1abd0
Christian <christianmaffs@gmail.com>
2025-05-05 23:59:12 +0200

    Search functions and pagination

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,18 @@ import React, { useState, useEffect } from 'react'
 
 import Cards from "./components/Cards/Cards"
 import Filters from "./components/Filters/Filters"
+import Pagination from './components/Pagination/Pagination';
+import Search from './components/Search/Search';
 
 function App() {
 
   let [pageNumber, setPageNumber] = useState(1);
+  let [search , setSearch] = useState('');
   let [fetchedData, updateFetctedData] = useState([]);
   let {info, results} = fetchedData;
 
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
 
 
   useEffect(()=> {
@@ -26,6 +29,8 @@ function App() {
     <>
     <h1 className="text-center ubuntu my-4">Rick & Morty <span className="text-primary">WiKi</span> </h1>
 
+    <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+
     <div className="container">
       <div className="row">
         <div className="col-3">
@@ -39,7 +44,7 @@ function App() {
       </div>
     </div>
 
-
+    <Pagination  pageNumber={pageNumber} setPageNumber={setPageNumber} />
     </>
   )
 }
